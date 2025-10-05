6c1fa4c15b8da90a9bcff1338a44696c0c0b14da
Willem Van Onsem <yourfriends@hapytex.eu>
2025-10-05 17:36:05 +0200

    fix test

--- timetest.py
+++ timetest.py
@@ -20,6 +20,10 @@ class Decade(TimeunitKind):
                 date: The first day (January 1) of the decade in which `dt` falls.
         """
         return date(10 * (dt.year // 10), 1, 1)
+    
+    @classmethod
+    def get_index_for_date(cls, dt):
+        return dt.year // 10
 
     @classmethod
     def last_day(cls, dt):
